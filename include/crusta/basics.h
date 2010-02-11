4c4330c5a53f5f25cf10842bf5e66d177fe4d80b
Tony Bernardin <tbernardin@ucdavis.edu>
2010-02-11 13:55:09 -0800

    Made INV_SPHEROID_RADIUS dependent on SPHEROID_RADIUS.

--- include/crusta/basics.h
+++ include/crusta/basics.h
@@ -52,7 +52,7 @@ typedef Geometry::Vector<Scalar, 3> Vector3;
 
 const uint   TILE_RESOLUTION     = 65;
 const double SPHEROID_RADIUS     = 6371000.0;
-const double INV_SPHEROID_RADIUS = 1.0 / 6371000.0;
+const double INV_SPHEROID_RADIUS = 1.0 / SPHEROID_RADIUS;
 
 END_CRUSTA
 
