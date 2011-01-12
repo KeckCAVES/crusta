c366d440a7109a89c5c3e5294697f3a18fd1eedc
Tony Bernardin <tbernardin@ucdavis.edu>
2011-01-11 19:28:00 -0800

    Generated the default desktop configuration

--- build/Install.cmake
+++ build/Install.cmake
@@ -8,5 +8,6 @@ set(CRUSTA_SHARES ../share/mapSymbolAtlas.tga
                   ../share/mapSymbols.cfg
                   ../share/decoratedRenderer.fp
                   ../share/plainRenderer.fp
+                  ../share/desktop.cfg
 )
 install(FILES ${CRUSTA_SHARES} DESTINATION ${CRUSTA_SHARE_PATH})
