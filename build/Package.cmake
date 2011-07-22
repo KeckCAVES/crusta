8b2c893884943e1c97b74d408e13dc155ad38720
Tony Bernardin <tbernardin@ucdavis.edu>
2011-07-21 18:37:01 -0700

    Added the package building facility back in (at least for linux)

--- build/Package.cmake
+++ build/Package.cmake
@@ -1,10 +1,10 @@
 # Setup package name, version and description
 set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
 
-set(CPACK_PACKAGE_VERSION_MAJOR ${${PROJECT_NAME}_MAJOR_VERSION})
-set(CPACK_PACKAGE_VERSION_MINOR ${${PROJECT_NAME}_MINOR_VERSION})
-set(CPACK_PACKAGE_VERSION_PATCH ${${PROJECT_NAME}_PATCH_VERSION})
-set(CPACK_PACKAGE_VERSION ${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH})
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_MAJOR_VERSION})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_MINOR_VERSION})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_PATCH_VERSION})
+set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
 
 set(CPACK_PACKAGE_VENDOR "keckcaves.org")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY
@@ -15,61 +15,30 @@ set(CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/readme.txt")
 
 
 # Setup the system name
-if(APPLE)
-   if (CMAKE_SIZEOF_VOID_P EQUAL 4)
-       set(CPACK_SYSTEM_NAME macosx32)
-   else()
-       set(CPACK_SYSTEM_NAME macosx64)
-   endif()
+if (CMAKE_SIZEOF_VOID_P EQUAL 4)
+  set(CPACK_SYSTEM_NAME Linux-i386)
 else()
-   if (CMAKE_SIZEOF_VOID_P EQUAL 4)
-      set(CPACK_SYSTEM_NAME Linux-i386)
-   else()
-      set(CPACK_SYSTEM_NAME Linux-amd64)
-   endif()
+  set(CPACK_SYSTEM_NAME Linux-amd64)
 endif()
 
 
 # Setup the generators
-if (APPLE)
-    set(CPACK_GENERATOR Bundle)
+set(CPACK_GENERATOR DEB;RPM)
 
-    set(CPACK_INSTALL_PREFIX "")
-    set(CPACK_PACKAGE_FILE_NAME ${PROJECT_NAME}-${CPACK_PACKAGE_VERSION})
-    set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/Crusta.icns")
-
-    set(CPACK_BUNDLE_NAME ${PROJECT_NAME})
-    set(CPACK_BUNDLE_ICON "${CMAKE_SOURCE_DIR}/Crusta.icns")
-    set(CPACK_BUNDLE_PLIST "${CMAKE_SOURCE_DIR}/Info.plist")
-    set(CPACK_BUNDLE_STARTUP_COMMAND "${CMAKE_SOURCE_DIR}/Crusta.scpt")
-
-#what do I actually pass to the <apps> of fixup_bundle
-#    install(CODE "include(BundleUtilities)
-#                  fixup_bundle(\"\${CMAKE_INSTALL_PREFIX}/bin/crustaDesktop\" \"\" \"\")")
+if (CMAKE_SIZEOF_VOID_P EQUAL 4)
+  set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE i386)
+  set(CPACK_RPM_PACKAGE_ARCHITECTURE i386)
 else()
-   set(CPACK_GENERATOR TGZ;DEB;RPM)
-
-   if (CMAKE_SIZEOF_VOID_P EQUAL 4)
-      set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE i386)
-      set(CPACK_RPM_PACKAGE_ARCHITECTURE i386)
-   else()
-      set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE amd64)
-      set(CPACK_RPM_PACKAGE_ARCHITECTURE x86_64)
-   endif()
-
-   set(CPACK_DEBIAN_PACKAGE_MAINTAINER tbernardin@ucdavis.edu)
+  set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE amd64)
+  set(CPACK_RPM_PACKAGE_ARCHITECTURE x86_64)
 endif()
 
-# Keep the CMake install prefix as is
-set(CPACK_SET_DESTDIR ON)
-
-
-# Setup Apple specific properties
-set(CPACK_OSX_PACKAGE_VERSION "10.6")
-
-
 # Setup debian specific properties
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgdal1-1.7.0 | libgdal1-1.6.0")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER tbernardin@ucdavis.edu)
+set(CPACK_DEBIAN_PACKAGE_SECTION science)
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgdal1-1.7.0 | libgdal1-1.6.0, vrui (>= 2.1)")
+#file(READ ${CPACK_PACKAGE_DESCRIPTION_FILE} CPACK_DEBIAN_PACKAGE_DESCRIPTION)
+
 
 # Include the Cpack module
 include(CPack)
