b7513a89c5a20c288b0a224007a9ad0ebfa8f226
Braden <contact@dabrado.net>
2012-02-16 18:50:41 -0800

    Add share path to list of paths to search for resources.

--- source/crusta/ResourceLocator.cpp
+++ source/crusta/ResourceLocator.cpp
@@ -13,6 +13,7 @@ init(const std::string& exePath, const std::string& resourcePath)
     if (!resourcePath.empty())
         addPath(resourcePath);
     addPathFromApplication(exePath);
+    addPath(CRUSTA_SHARE_PATH);
 }
 
 ResourceLocator RESOURCELOCATOR;
